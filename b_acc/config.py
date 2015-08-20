b5914e6a52b756ef455dbc7a1131d2e79a111def
frad00r4 <frad00r4@gmail.com>
2015-08-20 10:03:15 +0300

    Delete config.py

--- b_acc/config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-DEBUG = True
-SECRET_KEY = 'dfgghgdsfgjfdfdfhgfdfsdsaaaaaa'
-WTF_CSRF_ENABLED = True
-WTF_CSRF_SECRET_KEY = 'dgfghgfhgdsferfdfrt654776utrfdgtjttehtrjgfh'
-LISTEN = '0.0.0.0'
-PORT = 5000
-SQLALCHEMY_DATABASE_URI = 'mysql://b_acc:hnmk83q!1@ec2-52-28-60-228.eu-central-1.compute.amazonaws.com:3306/b_acc'
